add04bec67f0ef169fca1ba5f712375d018a84f5
CordlessCoder <>
2023-05-13 21:24:38 +0100

    improved multithreaded version drastically

--- rust_multithread/src/main.rs
+++ rust_multithread/src/main.rs
@@ -54,59 +54,6 @@ fn main() -> io::Result<()> {
     thread::scope(|s| {
         let dictionary = &dictionary;
         let mut thread_list = Vec::with_capacity(threads);
-        {
-            let thread_partition = unsafe { input_map.get_unchecked(0..per_thread) };
-            // First thread
-            let thread = s.spawn(move || {
-                let mut out = Vec::with_capacity(thread_partition.len());
-                let mut start = 0;
-                for (idx, &byte) in thread_partition.into_iter().enumerate() {
-                    if !byte.is_ascii_alphabetic() {
-                        // Terminate word
-                        if start < idx {
-                            let original_word = &thread_partition[start..idx];
-                            let len = out.len();
-                            // Add the word to the buffer, so we can convert it to lowercase
-                            out.extend_from_slice(original_word);
-                            let word = &mut out[len..];
-                            word.make_ascii_lowercase();
-                            // Get the new word
-                            let new_word = dictionary
-                                .get(word)
-                                .map(|w| w.as_slice())
-                                .unwrap_or(original_word);
-                            // Remove the word we converted to lowercase from the output buffer
-                            out.truncate(len);
-                            // Add the converted/original word into the output buffer
-                            out.extend_from_slice(new_word);
-                        }
-                        // Write terminating byte
-                        start = idx + 1;
-                        out.push(byte)
-                    }
-                }
-                // Ignore word overflow if there's multiple threads as it'll be handled by the following thread
-                if threads == 1 {
-                    let original_word = &thread_partition[start..];
-                    let len = out.len();
-                    // Add the word to the buffer, so we can convert it to lowercase
-                    out.extend_from_slice(original_word);
-                    let word = &mut out[len..];
-                    word.make_ascii_lowercase();
-                    // Get the new word
-                    let new_word = dictionary
-                        .get(word)
-                        .map(|w| w.as_slice())
-                        .unwrap_or(original_word);
-                    // Remove the word we converted to lowercase from the output buffer
-                    out.truncate(len);
-                    // Add the converted/original word into the output buffer
-                    out.extend_from_slice(new_word);
-                }
-                out
-            });
-            thread_list.push(thread)
-        }
         let lastthread = threads - 1;
         for thread in 1..threads {
             let start = per_thread * thread;
@@ -186,6 +133,63 @@ fn main() -> io::Result<()> {
             });
             thread_list.push(thread)
         }
+        let first_out = {
+            let thread_partition = unsafe { input_map.get_unchecked(0..per_thread) };
+            // Use main as first "thread"
+            let mut out = Vec::with_capacity(thread_partition.len());
+            let mut start = 0;
+            for (idx, &byte) in thread_partition.into_iter().enumerate() {
+                if !byte.is_ascii_alphabetic() {
+                    // Terminate word
+                    if start < idx {
+                        let original_word = &thread_partition[start..idx];
+                        let len = out.len();
+                        // Add the word to the buffer, so we can convert it to lowercase
+                        out.extend_from_slice(original_word);
+                        let word = &mut out[len..];
+                        word.make_ascii_lowercase();
+                        // Get the new word
+                        let new_word = dictionary
+                            .get(word)
+                            .map(|w| w.as_slice())
+                            .unwrap_or(original_word);
+                        // Remove the word we converted to lowercase from the output buffer
+                        out.truncate(len);
+                        // Add the converted/original word into the output buffer
+                        out.extend_from_slice(new_word);
+                    }
+                    // Write terminating byte
+                    start = idx + 1;
+                    out.push(byte)
+                }
+            }
+            // Ignore word overflow if there's multiple threads as it'll be handled by the following thread
+            if threads == 1 {
+                let original_word = &thread_partition[start..];
+                let len = out.len();
+                // Add the word to the buffer, so we can convert it to lowercase
+                out.extend_from_slice(original_word);
+                let word = &mut out[len..];
+                word.make_ascii_lowercase();
+                // Get the new word
+                let new_word = dictionary
+                    .get(word)
+                    .map(|w| w.as_slice())
+                    .unwrap_or(original_word);
+                // Remove the word we converted to lowercase from the output buffer
+                out.truncate(len);
+                // Add the converted/original word into the output buffer
+                out.extend_from_slice(new_word);
+            }
+            out
+        };
+        output
+            .write_all(&first_out)
+            .map_err(|err| {
+                eprintln!("Failed to write to output file");
+                err
+            })
+            .unwrap();
         for thread in thread_list {
             let finished = thread
                 .join()
